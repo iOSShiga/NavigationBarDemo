b5dd9648a525f44ae9a71d7fdb53e506d6e7158a
Shigasuresh@123 <shigasuresh546@gmail.com>
2020-02-06 23:56:27 +0530

    Navigation customization...

--- NavigationBarDemo/ContentView.swift
+++ NavigationBarDemo/ContentView.swift
@@ -9,8 +9,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // NavigationBar Customization
+    
+    init() {
+        UINavigationBar.appearance().backgroundColor = .systemPink
+        UINavigationBar.appearance().largeTitleTextAttributes = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont(name: "AlNile-Bold", size: 40)!
+        ]
+        
+        UINavigationBar.appearance().barTintColor = .blue
+        UINavigationBar.appearance().titleTextAttributes = [
+            .foregroundColor: UIColor.white,
+                .font: UIFont(name: "AlNile-Bold", size: 20)!
+        ]
+        
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+            
+            List {
+                
+                ForEach (0 ..< 25) { _ in
+            Text("Hello, World!")
+            }
+        }
+            .navigationBarTitle(Text("Navigation Bar"), displayMode: .large)
+            
+            .navigationBarItems(leading: Button(action: {
+                print("Share")
+            }, label: {
+                Image(systemName: "trash.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+            }),
+            
+                    trailing: Button(action: {
+                            print("folder")
+                                }, label: {
+                                    Image(systemName: "folder")
+                                        .font(.largeTitle)
+                                        .foregroundColor(.white)
+                        }))
+//            .navigationBarItems(leading: EditButton())
+//            .navigationBarItems(trailing: EditButton())
+            
+        }
     }
 }
 
