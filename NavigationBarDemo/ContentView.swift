764035a998cac01123f58f64f9638bc0a133ef72
Shigasuresh@123 <shigasuresh546@gmail.com>
2020-02-06 22:39:20 +0530

    Initial Commit

---
+++ NavigationBarDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationBarDemo
+//
+//  Created by shiga on 06/02/20.
+//  Copyright © 2020 shiga. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
